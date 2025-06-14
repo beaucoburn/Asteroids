d7a19b8fd7f4f6c5018cea179093d8394c4cb158
beaucoburn <douglas.coburn@gmail.com>
2025-06-14 10:29:59 -0600

    new file:   __pycache__/constants.cpython-313.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    fprint(f"Screen width: {SCREEN_WIDTH}")
-    fprint(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
