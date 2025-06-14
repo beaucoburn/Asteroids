2af8e4b70a7a3c23f7ed12f236f26e0cc1a89673
beaucoburn <douglas.coburn@gmail.com>
2025-06-14 11:04:49 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
