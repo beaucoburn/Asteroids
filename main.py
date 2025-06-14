3fc96e709ebbdd81c09425887862d3d2b0501467
beaucoburn <douglas.coburn@gmail.com>
2025-06-14 11:39:54 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        fill(black)
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    display.flip()
 
 if __name__ == "__main__":
     main()
