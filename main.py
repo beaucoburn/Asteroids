c4cb26cbc5e987acae53dbd347d2dc359bd24353
beaucoburn <douglas.coburn@gmail.com>
2025-06-14 10:28:09 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    fprint(f"Screen width: {SCREEN_WIDTH}")
+    fprint(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
