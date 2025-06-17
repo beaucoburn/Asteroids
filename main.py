7077f2054c356c66088827f268a2b54d6dbe9b02
beaucoburn <douglas.coburn@gmail.com>
2025-06-17 10:53:58 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     updatable.add(player)
