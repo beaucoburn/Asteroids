43490a88787d318edfb73e6b3b0ad1731f1374dd
beaucoburn <douglas.coburn@gmail.com>
2025-06-17 09:37:19 -0600

    modified:   player.py

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(CircleShape):
     def triangle(self):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
         right = pygame.Vector2(0,1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.postion + forward * self.radius
+        a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
